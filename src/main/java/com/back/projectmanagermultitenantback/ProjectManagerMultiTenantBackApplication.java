0ccc53173bde4e109f86d1d0c7dffc0378d9148b
Andriantsitohaina Hajaina <74816899+mamyhajaina@users.noreply.github.com>
2025-11-01 12:35:58 +0300

    initialize the project

---
+++ src/main/java/com/back/projectmanagermultitenantback/ProjectManagerMultiTenantBackApplication.java
@@ -0,0 +1,13 @@
+package com.back.projectmanagermultitenantback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagerMultiTenantBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagerMultiTenantBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/projectmanagermultitenantback/ProjectManagerMultiTenantBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.projectmanagermultitenantback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagerMultiTenantBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
