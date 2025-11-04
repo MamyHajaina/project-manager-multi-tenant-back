d22b1169c4b09566bd96e20d2fe8e39f572343bc
Andriantsitohaina Hajaina <74816899+mamyhajaina@users.noreply.github.com>
2025-11-04 15:47:14 +0300

    Fix cors

--- src/main/java/com/back/projectmanagermultitenantback/config/SecurityConfig.java
+++ src/main/java/com/back/projectmanagermultitenantback/config/SecurityConfig.java
@@ -75,7 +75,9 @@ public class SecurityConfig {
         // Origines autorisées : ton poste Linux + localhost
         cfg.setAllowedOriginPatterns(List.of(
                 "http://192.168.88.21:4200",
-                "http://localhost:4200"
+                "http://localhost:4200",
+                "https://project-manager-multi-tenant-back.onrender.com",
+                "https://project-manager-mutli-tenant-front.onrender.com"
         ));
         // Méthodes autorisées
         cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
